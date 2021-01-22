fc838cb01505e6510399722a7d4a36d8ff8801a9
Andrei Kazakov <ay.kazakov89@gmail.com>
2021-01-22 18:55:02 +0100

    Reduce speed

--- src/player.rs
+++ src/player.rs
@@ -8,18 +8,18 @@ impl Player {
     pub fn turn_left(&mut self) {
         self.angle -= 0.1
     }
-    
+
     pub fn turn_right(&mut self) {
         self.angle += 0.1
     }
-    
+
     pub fn move_forward(&mut self) {
-        self.x += self.angle.sin();
-        self.y += self.angle.cos();
+        self.x += self.angle.sin() * 0.5;
+        self.y += self.angle.cos() * 0.5;
     }
-    
+
     pub fn move_backwards(&mut self) {
-        self.x -= self.angle.sin();
-        self.y -= self.angle.cos();
+        self.x -= self.angle.sin() * 0.5;
+        self.y -= self.angle.cos() * 0.5;
     }
 }
